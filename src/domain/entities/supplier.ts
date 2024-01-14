1bc82210a486a2318afcd5bc778deb4cb608f9b3
gabrielppd77 <gabrielppd77@outlook.com>
2024-01-14 16:08:14 -0300

    adjust in entities category and supplier, add fields createdAt and updatedAt in domain

--- src/domain/entities/supplier.ts
+++ src/domain/entities/supplier.ts
@@ -1,14 +1,43 @@
 import { Entity } from '@core/common/entities/entity';
 
+interface SupplierPropsOverride
+  extends Omit<SupplierProps, 'status' | 'createdAt' | 'updatedAt'> {
+  createdAt?: string;
+  updatedAt?: string;
+}
+
 interface SupplierProps {
   name: string;
+
+  createdAt: string;
+  updatedAt: string;
 }
 
 export class Supplier extends Entity<SupplierProps> {
+  constructor(props: SupplierPropsOverride, id?: string) {
+    super(
+      {
+        ...props,
+        createdAt: props.createdAt ? props.createdAt : new Date().toISOString(),
+        updatedAt: props.updatedAt ? props.updatedAt : new Date().toISOString(),
+      },
+      id,
+    );
+  }
+
   get name() {
     return this.props.name;
   }
+
   set name(name: string) {
     this.props.name = name;
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
+
+  get updatedAt() {
+    return this.props.createdAt;
   }
 }
