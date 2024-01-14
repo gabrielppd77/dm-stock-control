c07e4cdc3d413e777f1847cf205ac16c9e7d7f28
gabrielppd77 <gabrielppd77@outlook.com>
2024-01-14 13:38:50 -0300

    add fieids createdAt and updatedAt in all entities

---
+++ prisma/migrations/20240114160931_create_fields_created_at_and_updated_at_for_all_entities/migration.sql
@@ -0,0 +1,11 @@
+-- AlterTable
+ALTER TABLE "categories" ADD COLUMN     "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3);
+
+-- AlterTable
+ALTER TABLE "products" ADD COLUMN     "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3);
+
+-- AlterTable
+ALTER TABLE "suppliers" ADD COLUMN     "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3);
