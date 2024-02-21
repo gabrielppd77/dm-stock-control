c3f06f3bc2af036e408a19587f5b2640611b84f1
gabrielppd77 <gabrielppd77@outlook.com>
2024-02-21 20:18:11 -0300

    update pagination, add startindex and endIndex in response of pagination

--- src/infra/modules/http/presenters/pagination.presenter.ts
+++ src/infra/modules/http/presenters/pagination.presenter.ts
@@ -3,12 +3,18 @@ export class PageOptionsPresenter {
   readonly size: number;
   readonly lastPage: number;
   readonly page: number;
+  readonly startIndex: number;
+  readonly endIndex: number;
 
   constructor(total: number, size: number, page: number) {
+    const auxEndIndex = page * size + (size - 1);
+
     this.length = total;
     this.size = size;
     this.lastPage = Math.ceil(total / size) - 1;
     this.page = page;
+    this.startIndex = page * size;
+    this.endIndex = auxEndIndex >= total ? total - 1 : auxEndIndex;
   }
 }
 
